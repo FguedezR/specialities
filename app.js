32a3474321451a7cd1b6106f712f0c90234dac7a
Fernando <fernanguedezr@gmail.com>
2026-01-17 13:38:16 +0100

    update app.js and users.js

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
 const express = require("express");
-const app     = express();
+const app = express();
+const { getUsers } = require("./users");
 
 app.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type": 'text/html; charset="UTF-8"' });
-  res.end(
-    '<h1>Home</h1><a href="/developers">Developers</a> | <a href="/marketing">Marketing</a> | <a href="/ventas">Ventas</a> | <a href="/qas">QAs</a>'
-  );
+  res.send(`
+    <h1>Home</h1>
+    <nav>
+        <a href="/specialty/developers">Developers</a> |
+        <a href="/specialty/marketing">Marketing</a> |
+        <a href="/specialty/ventas">Ventas</a> |
+        <a href="/specialty/qas">QAs</a>
+    </nav>
+  `);
 });
 
 app.get("/developers", (req, res) => {
-    res.end('<h1>Developers</h1><a href="/">Home</a>');
+  const developers = getUsers("developers");
+  const usersList = developers
+    .map((u) => `<li>${u.name}, ${u.age}</li>`)
+    .join("");
+  res.send(`
+        <h1>Developers</h1>
+        <a href="/">Home</a>
+        <ul>${usersList}</ul>
+        `);
 });
 
-app.get("/marketing", (req, res) => {
-  res.end('<h1>Marketing</h1><a href="/">Home</a>');
-});
+// ruta dinamica
+app.get("/specialty/:type", (req, res) => {
+  const type = req.params.type;
+  const filteredUsers = getUsers(type);
 
-app.get("/ventas", (req, res) => {
-  res.end('<h1>Ventas</h1><a href="/">Home</a>');
-});
+  if (filteredUsers.length === 0) {
+    return res
+      .status(404)
+      .send('<h1>Especialidad no encontrada</h1><a href="/">Home</a>');
+  }
 
-app.get("/qas", (req, res) => {
-  res.end('<h1>QAs</h1><a href="/">Home</a>');
+  const usersList = filteredUsers
+    .map((u) => `<li>${u.name} (${u.age} años)</li>`)
+    .join(""); // Importante: añade .join("") para evitar las comas del array
+
+  res.send(`
+    <h1>Especialidad: ${type.toUpperCase()}</h1>
+    <a href="/">Volver a la Home</a>
+    <ul>${usersList}</ul>
+  `);
 });
 
 // manejo de errores
@@ -32,5 +56,5 @@ app.use((req, res) => {
 // puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Escuchando en el puerto http://localhost:${PORT}`);
+  console.log(`Servidor funcionando en el puerto http://localhost:${PORT}`);
 });
