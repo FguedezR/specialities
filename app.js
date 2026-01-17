2ee246a0072b5bbeed9aac99dec73c9d7442a497
Fernando <fernanguedezr@gmail.com>
2026-01-17 12:00:19 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require("express");
-const app = express();
+const app     = express();
 
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": 'text/html; charset="UTF-8"' });
-  res.end("<h1>Home</h1>");
+  res.end(
+    '<h1>Home</h1><a href="/marketing">Marketing</a> | <a href="/developers">Developers</a> | <a href="/ventas">Ventas</a>'
+  );
 });
 
 app.get("/marketing", (req, res) => {
-  res.end("<h1>Marketing</h1>");
+  res.end('<h1>Marketing</h1><a href="/">Home</a>');
 });
 
+app.get("/developers", (req, res) => {
+  res.end('<h1>Developers</h1><a href="/">Home</a>');
+});
 
+app.get("/ventas", (req, res) => {
+  res.end('<h1>Ventas</h1><a href="/">Home</a>');
+});
 
 // manejo de errores
 app.use((req, res) => {
-    res.status(404).send('<h1>Página no encontrada</h1><a href="/">Home</a>')
-})
+  res.status(404).send('<h1>Página no encontrada</h1><a href="/">Home</a>');
+});
 
 // puerto
 const PORT = process.env.PORT || 3000;
