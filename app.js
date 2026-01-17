89b3e0e2ed50a6684837022e3811afd0f763c855
Fernando <fernanguedezr@gmail.com>
2026-01-17 12:07:28 +0100

    update and create pages in server app.js

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ const app     = express();
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": 'text/html; charset="UTF-8"' });
   res.end(
-    '<h1>Home</h1><a href="/marketing">Marketing</a> | <a href="/developers">Developers</a> | <a href="/ventas">Ventas</a>'
+    '<h1>Home</h1><a href="/developers">Developers</a> | <a href="/marketing">Marketing</a> | <a href="/ventas">Ventas</a> | <a href="/qas">QAs</a>'
   );
+});
+
+app.get("/developers", (req, res) => {
+    res.end('<h1>Developers</h1><a href="/">Home</a>');
 });
 
 app.get("/marketing", (req, res) => {
   res.end('<h1>Marketing</h1><a href="/">Home</a>');
 });
 
-app.get("/developers", (req, res) => {
-  res.end('<h1>Developers</h1><a href="/">Home</a>');
-});
-
 app.get("/ventas", (req, res) => {
   res.end('<h1>Ventas</h1><a href="/">Home</a>');
+});
+
+app.get("/qas", (req, res) => {
+  res.end('<h1>QAs</h1><a href="/">Home</a>');
 });
 
 // manejo de errores
